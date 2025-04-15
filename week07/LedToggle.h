82cefa4d9e5c73c3011bd245cd852126fdbf5ea7
버드텐 <163097170+0215dbwkd@users.noreply.github.com>
2025-04-15 11:21:27 +0900

    Create LedToggle.h

---
+++ week07/LedToggle.h
@@ -0,0 +1,17 @@
+#ifndef LedToggle_h
+#define LedToggle_h
+
+#include "Arduino.h"
+
+class LedToggle
+{
+    public:
+        LedToggle(int pin); //생성자
+        void toggle( );      //LED 상태 토글글
+    private:
+        int _pin;
+        bool _state;
+        
+};
+
+#endif
